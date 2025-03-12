1c62bc721c59fc140d1faa49311aceebdf4b939d
Kanwaljeet Singh Kalra <4933214+KKS1@users.noreply.github.com>
2025-03-12 11:16:35 -0600

    .

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -22,10 +22,14 @@ export async function createInvoice(formData: FormData) {
   amount *= 100; // convert to cents
   const date = new Date().toISOString().split('T')[0];
 
-  await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    values (${customerId}, ${amount}, ${status}, ${date})
-  `;
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      values (${customerId}, ${amount}, ${status}, ${date})
+    `;
+  } catch (error) {
+    console.error(error)
+  }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
@@ -37,12 +41,28 @@ export async function updateInvoice(id: string, formData: FormData) {
   amount *= 100; // convert to cents
   const date = new Date().toISOString().split('T')[0];
 
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amount}, status = ${status}
-    WHERE id = ${id}
-  `;
-
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amount}, status = ${status}
+      WHERE id = ${id}
+    `;
+  } catch (error) {
+    console.error(error)
+  }
+   
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  try {
+    await sql`
+      DELETE FROM invoices WHERE id = ${id}
+    `;
+
+    revalidatePath('/dashboard/invoices');
+  } catch (error) {
+    console.error(error)
+  }
 }
