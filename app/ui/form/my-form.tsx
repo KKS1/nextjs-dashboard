c90031782b303d2777570f2b03687a5ca4c02ed7
Kanwaljeet Singh Kalra <4933214+KKS1@users.noreply.github.com>
2025-05-02 13:45:03 -0600

    wip

--- app/ui/form/my-form.tsx
+++ app/ui/form/my-form.tsx
@@ -1,28 +1,30 @@
-'use client'
-import { useActionState, useEffect, useRef } from 'react'
-import Grid from '@mui/material/Grid'
-import { Controller, useForm } from 'react-hook-form'
-import { TextField } from '@mui/material'
+'use client';
+import { useActionState, useEffect } from 'react';
+import Grid from '@mui/material/Grid';
+import { Controller, useForm } from 'react-hook-form';
+import { Button, TextField } from '@mui/material';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { submitMyForm } from '@/app/lib/actions'
-import { MyFormSchema } from '@/app/lib/definitions'
+import { submitMyForm } from '@/app/lib/actions';
+import { MyFormSchema } from '@/app/lib/definitions';
 
 const initialState = {
   success: '',
   error: '',
-}
+};
 
 const MyForm = () => {
-  const formRef = useRef<HTMLFormElement>(null)
-  const [state, formAction, isPending] = useActionState(submitMyForm, initialState)
+  const [state, formAction, isPending] = useActionState(
+    submitMyForm,
+    initialState
+  );
 
   useEffect(() => {
     if (state?.success) {
-      console.log(state.success)
+      console.log(state.success);
     } else if (state?.error) {
-      console.error(state.error)
+      console.error(state.error);
     }
-  }, [state])
+  }, [state]);
 
   const form = useForm({
     resolver: zodResolver(MyFormSchema),
@@ -35,19 +37,18 @@ const MyForm = () => {
 
   return (
     <form
-      ref={formRef}
       action={formAction}
-      onSubmit={form.handleSubmit(() => formRef?.current?.submit())}
+      onSubmit={form.handleSubmit((_, e) => e?.target.submit())}
     >
       <Grid container spacing={6}>
         <Grid size={{ xs: 12, md: 12 }}>
           <Controller
-            name='userName'
+            name="userName"
             control={form.control}
             render={({ field, fieldState }) => (
               <TextField
                 {...field}
-                label='User Name'
+                label="User Name"
                 error={!!fieldState.error}
                 helperText={fieldState.error?.message}
               />
@@ -56,12 +57,12 @@ const MyForm = () => {
         </Grid>
         <Grid size={{ xs: 12, md: 12 }}>
           <Controller
-            name='email'
+            name="email"
             control={form.control}
             render={({ field, fieldState }) => (
               <TextField
                 {...field}
-                label='Email'
+                label="Email"
                 error={!!fieldState.error}
                 helperText={fieldState.error?.message}
               />
@@ -69,12 +70,18 @@ const MyForm = () => {
           />
         </Grid>
         <Grid size={{ xs: 12, md: 12 }}>
-          <input type="submit" disabled={isPending} value={isPending ? 'Submitting...' : 'Submit'} />
+          <Button
+            variant="contained"
+            type="submit"
+            disabled={isPending}
+            value={isPending ? 'Submitting...' : 'Submit'}
+          >
+            {isPending ? 'Submitting...' : 'Submit'}
+          </Button>
         </Grid>
       </Grid>
     </form>
+  );
+};
 
-  )
-}
-
-export default MyForm
+export default MyForm;
